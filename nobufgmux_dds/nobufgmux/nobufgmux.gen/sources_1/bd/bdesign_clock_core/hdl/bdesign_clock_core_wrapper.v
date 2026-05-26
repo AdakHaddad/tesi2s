a1381972a67b359cad8e0abf5583b26a0bc2ea6c
adakhaddad <muhammadmuqtada@gmail.com>
2026-05-26 21:52:06 +0700

    Implement I2S DDS generator and updated hardware/software projects

---
+++ nobufgmux_dds/nobufgmux/nobufgmux.gen/sources_1/bd/bdesign_clock_core/hdl/bdesign_clock_core_wrapper.v
@@ -0,0 +1,49 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2025.2 (win64) Build 6299465 Fri Nov 14 19:35:11 GMT 2025
+//Date        : Tue May 26 21:16:18 2026
+//Host        : DESKTOP-HTVV1N1 running 64-bit major release  (build 9200)
+//Command     : generate_target bdesign_clock_core_wrapper.bd
+//Design      : bdesign_clock_core_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module bdesign_clock_core_wrapper
+   (i2s_bclk_0,
+    i2s_data_0,
+    i2s_mclk_0,
+    i2s_ws_0,
+    reset,
+    sys_clock,
+    usb_uart_rxd,
+    usb_uart_txd);
+  output i2s_bclk_0;
+  output i2s_data_0;
+  output i2s_mclk_0;
+  output i2s_ws_0;
+  input reset;
+  input sys_clock;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire i2s_bclk_0;
+  wire i2s_data_0;
+  wire i2s_mclk_0;
+  wire i2s_ws_0;
+  wire reset;
+  wire sys_clock;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  bdesign_clock_core bdesign_clock_core_i
+       (.i2s_bclk_0(i2s_bclk_0),
+        .i2s_data_0(i2s_data_0),
+        .i2s_mclk_0(i2s_mclk_0),
+        .i2s_ws_0(i2s_ws_0),
+        .reset(reset),
+        .sys_clock(sys_clock),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+endmodule
